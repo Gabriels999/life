60ca2d1e408720e5106639a41a2fef9e6c00aa22
Gabriel Silva Santos <gabriel.santos@buser.com.br>
2022-12-23 19:26:26 -0300

    reestruturacao

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,16 +2,63 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+    <div v-for="item, index_row in matriz" :key="index_row" class="flex">
+      <div v-for="item, index_col in matriz[index_row]" :key="index_col">
+        <CelulaLife :row="index_row" :col="index_col" @update-life="updateCelula($event)" :item="item"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import CelulaLife from './components/CelulaLife'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CelulaLife
+  },
+  data() {
+    return {
+      cols: 4,
+      rows: 4,
+      matriz: [
+        [1,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,1,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0]
+      ],
+      alive: false
+    }
+  },
+  methods: {
+    init(){
+
+    },
+    updateCelula(event){
+      if (event[2] == 0){
+        this.matriz[event[0]][event[1]] = 1
+        console.log(this.matriz[event[0]][event[1]] = 1)
+        debugger
+      } else {
+        this.matriz[event[0]][event[1]] = 0
+      }
+      //this.matriz[row][column] = newValue
+      //this.$forceUpdate()
+    }
+  },
+  watch:{
+    matriz(){
+      return this.matriz
+    }
   }
 }
 </script>
@@ -24,5 +71,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.flex {
+  display: flex;
 }
 </style>
---
+++ vue/src/components/CelulaLife.vue
@@ -0,0 +1,37 @@
+<template>
+    <button :class='{"box":true, "alive":item}' @click="changeState"></button>
+</template>
+
+<script>
+export default{
+    props:{
+        row: "",
+        col: "",
+        item: 0
+    },
+    methods:{
+        changeState(){
+            this.$emit('update-life', [this.row, this.col, this.item])
+        }
+    }
+}
+</script>
+
+<style>
+.box{
+    display: inline-block;
+    border: 1px solid gray;
+    padding: 8px;
+    color: white;
+    background-color: grey;
+    margin: 2px;
+}
+.alive{
+    color: green;
+    background-color: green;
+}
+.dead{
+    color: gray;
+    background-color: gray;
+}
+</style>
