de932cabbdad4cb931b7f6f407e63e92ef441c15
Gabriel Silva Santos <gabriel.santos@buser.com.br>
2023-01-20 15:29:43 -0300

    fix vizinhos

--- vue-app/src/service/vizinhos.js
+++ vue-app/src/service/vizinhos.js
@@ -16,20 +16,6 @@ export function verificaVizinhos(matriz, x, y) {
         if (matriz[item.x + x]?.[item.y + y] == 1) {
             vizinhosVivos += 1;
         }
-        // if (
-        //     i["x"] + x >= 0 &&
-        //     i["y"] + y >= 0 &&
-        //     i["x"] + x < matriz.length &&
-        //     i["y"] + y < matriz[0].length
-        // ) {
-        //     vizinhos.push({ x: i["x"] + x, y: i["y"] + y });
-        // }
     });
-
-    // vizinhos.forEach((i) => {
-    //     if (matriz[i["x"]][i["y"]] == 1) {
-    //         vizinhosVivos += 1;
-    //     }
-    // });
     return vizinhosVivos;
 }
